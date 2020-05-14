5b84275269abee3741f037e1cb17df98f4d8c30f
Rakib Hasan <rakib.hasan@bjitgroup.com>
2020-05-14 21:23:27 +0600

    initiat project

---
+++ src/main/java/com/extremecoder/springrestapi/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.extremecoder.springrestapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/api/hello")
+	public String hello() {
+		return "hello";
+	}
+}
